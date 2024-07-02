f6675753a2292fe3bdd89be122f6a647303e008d
natuliochek <natvicor@mail.ru>
2024-07-02 18:49:32 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { data } from './data';
 import './App.css';
 
 function App() {
+  const [thing, setThing] = useState(0);
+  const {id, name, image, price, description} = data[thing];
+  // console.log(data[thing])
+
+  const [showMore, setShowMore] = useState(false);
+
+  const previousThing = ()=> {
+    setThing((thing =>{
+     thing --;
+     if (thing < 0){
+      return data.length-1;
+     }
+     return thing; 
+    }))}
+
+  const nextThing = ()=> {
+    setThing((thing => {
+     thing ++;
+     if (thing > data.length -1){
+      thing = 0;
+     }
+    return thing; 
+    }))}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='container'>
+        <h1>List of { data.length } MaybeToBuy Things</h1>
+      </div>
+      <div className='container'>
+      <button onClick={ previousThing } className='btn'>Previous</button>
+      <button onClick={ nextThing } className='btn'>Next</button>
     </div>
+      <div>
+        <div className='container'>
+          <h3>{ id }</h3>
+        </div>
+        <div className='container'>
+          <h2>{ name }</h2>
+        </div>
+        <div className='container'>
+          <img src={ image } width="300px" alt="item"/>
+        </div>
+        <div className='container'>
+          <p className='describtion'>{showMore ? description : description.substring(0,50) + "..."}
+            <button className='btnShow' onClick={()=> setShowMore(!showMore)} >{ showMore ? "Show less" : "Show more" }</button>
+          </p>
+        </div>
+        <div className='container'>
+          <p className='price'>{ price }</p>
+        </div>
+        
+    </div>
+    
+    </div>
+    
   );
 }
 
